95e3a990aad00a78c7358ce209642ab063797df1
Sviatoslav Bilokin <bilokin@lx2.lal.in2p3.fr>
2015-02-18 18:16:13 +0100

    first working version. Fake rate 50%

--- include/VertexRestorer.hh
+++ include/VertexRestorer.hh
@@ -2,13 +2,22 @@
 #define VertexRestorer_h 1
 
 #include "marlin/Processor.h"
+#include <iomanip>
 #include <iostream>
 #include <stdlib.h>
 #include "lcio.h"
 #include "TFile.h"
 #include "TTree.h"
 #include <string>
-
+#include "MathOperator.hh"
+#include <EVENT/ReconstructedParticle.h>
+#include <IMPL/ReconstructedParticleImpl.h>
+#include <IMPL/VertexImpl.h>
+#include <EVENT/MCParticle.h>
+#include <UTIL/LCRelationNavigator.h>
+#include <EVENT/LCCollection.h>
+#include <EVENT/Vertex.h>
+#include <EVENT/Track.h>
 using namespace lcio ;
 using namespace marlin ;
 
@@ -24,38 +33,84 @@ using namespace marlin ;
  * @version $Id: VertexRestorer.h,v 1.0 2010/02/18 Exp $ 
  */
 
+namespace TTbarAnalysis
+{
+	class VertexRestorer : public Processor {
+	  
+	 public:
+	  
+	  virtual Processor*  newProcessor() { return new VertexRestorer ; }
+	  
+	  
+	  VertexRestorer() ;
+	  
+	  virtual void init() ;
+	  virtual void processRunHeader( LCRunHeader* run ) ;
+	  virtual void processEvent( LCEvent * evt ) ; 
+	  virtual void check( LCEvent * evt ) ; 
+	  virtual void end() ;
+	  
+	  void PrintTrack(EVENT::Track * track); 
+	  void PrintParticle(EVENT::ReconstructedParticle * particle);
 
-class VertexRestorer : public Processor {
-  
- public:
-  
-  virtual Processor*  newProcessor() { return new VertexRestorer ; }
-  
-  
-  VertexRestorer() ;
-  
-  virtual void init() ;
-  virtual void processRunHeader( LCRunHeader* run ) ;
-  virtual void processEvent( LCEvent * evt ) ; 
-  virtual void check( LCEvent * evt ) ; 
-  virtual void end() ;
-  
-  
- protected:
+	  bool CompareParticles(const EVENT::ReconstructedParticle * particle1, const EVENT::ReconstructedParticle * particle2); 
+	  bool CompareParticles(const EVENT::MCParticle * particle1, const EVENT::ReconstructedParticle * particle2); 
+	  void CompareCollections(std::vector< EVENT::ReconstructedParticle * > * detected, EVENT::LCCollection * missed, EVENT::LCCollection * bstar);
+	  std::vector< EVENT::ReconstructedParticle * > * RestoreVerticesPFO(EVENT::LCCollection * main, EVENT::LCCollection * sec); 
+	  std::vector< EVENT::ReconstructedParticle * > * RestoreVertices(EVENT::Vertex * primary, EVENT::LCCollection * sec); 
+	  std::vector< EVENT::ReconstructedParticle * > * RestoreVertices(EVENT::LCCollection * sec, EVENT::LCCollection * rel); 
+	  std::vector< EVENT::ReconstructedParticle * > GetAdditionalParticles(const std::vector< EVENT::ReconstructedParticle * > & pri, EVENT::Vertex * sec, const std::vector< EVENT::ReconstructedParticle * > * toCompare = NULL);
+	  float GetDeviation(const EVENT::ReconstructedParticle * particle, double * pos);
+	  float GetError(const EVENT::ReconstructedParticle * particle);
+	  EVENT::ReconstructedParticle * CopyParticle(const EVENT::ReconstructedParticle * particle, const EVENT::Vertex * vertex);
+	  float IsDublicate(const EVENT::ReconstructedParticle * particle, std::vector< EVENT::ReconstructedParticle * > & data);
+	 protected:
 
-  std::string _colName ;
-  std::string _clusterName ;
-  
+	  std::string _colName ;
+	  std::string _colPriName ;
+	  std::string _colSecName ;
+	  std::string _colJetName ;
+	  std::string _colJetRelName ;
+	  std::string _clusterName ;
+	  std::string _colMissedName ;
+	  std::string _colBStarName ;
+	 
+	  static const int MAXN = 15;
+	  float _fakeMomentum[MAXN];
+	  float _bstarMomentum[MAXN];
+	  float _missedMomentum[MAXN];
+	  
+	  float _fakeDeviation[MAXN];
+	  float _bstarDeviation[MAXN];
+	  float _missedDeviation[MAXN];
+	  float _fakeError[MAXN];
+	  float _bstarError[MAXN];
+	  float _missedError[MAXN];
+		
+	  double ip[3];
+	  float _aParameter;
+	  float _bParameter;
+	  
+	  int _missedDetected;
+	  int _bstarDetected;
+	  int _fakeDetected;
 
-  int _nRun ;
-  int _nEvt ;
-  
-  TFile* hfile ;
-  std::string _hfilename ;
-  TTree* _Tree;
-} ;
+	  int _missedTotal;
+	  int _bstarTotal;
+	  int _detectedTotal;
 
-#endif
+	  int _nRun ;
+	  int _nEvt ;
+
+
+	  float _offsetCut;
+
+	  TFile* hfile ;
+	  std::string _hfilename ;
+	  TTree* _Tree;
+	} ;
+}
+	#endif
 
 
 
--- src/VertexRestorer.cc
+++ src/VertexRestorer.cc
@@ -2,71 +2,461 @@
 
 using namespace lcio ;
 using namespace marlin ;
+using std::vector;
+using std::string;
+using std::abs;
+using EVENT::Track;
+using IMPL::ReconstructedParticleImpl;
+using IMPL::VertexImpl;
+namespace TTbarAnalysis
+{
+	VertexRestorer aVertexRestorer ;
 
 
-VertexRestorer aVertexRestorer ;
+	VertexRestorer::VertexRestorer() : Processor("VertexRestorer") {
+	  
+	  // modify processor description
+	  _description = "VertexRestorer";
+	  
+
+	  // register steering parameters: name, description, class-variable, default value
+	  
+	  registerProcessorParameter( "ROOTFileName",
+				      "Output ROOT File Name",
+				      _hfilename,
+				      string("VertexRestorer.root") );
+	  
+	  registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE , 
+				   "PFOCollection",
+				   "Name of the Calorimeter hit collection"  ,
+				   _colName ,
+				   string("PandoraPFOs") ) ;
+
+	  registerInputCollection( LCIO::VERTEX,
+	      "PrimaryCollectionName" , 
+	      "Name of the PrimaryVertex collection"  ,
+	      _colPriName ,
+	      std::string("PrimaryVertex")
+	   );
+	    registerInputCollection( LCIO::VERTEX,
+		    "SecondaryCollectionName" , 
+		    "Name of the BuildUpVertex collection"  ,
+		    _colSecName ,
+		    std::string("BuildUpVertex")
+	    );
+	    registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+		    "JetCollectionName" , 
+		    "Name of the Jet collection"  ,
+		    _colJetName ,
+		    std::string("FinalJets")
+	    );
+	    registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+		    "MissedCollectionName" , 
+		    "Name of the Jet collection"  ,
+		    _colMissedName ,
+		    std::string("MissedParticles")
+	    );
+	    registerInputCollection( LCIO::MCPARTICLE,
+		    "BStarCollectionName" , 
+		    "Name of the BStar collection"  ,
+		    _colBStarName ,
+		    std::string("BStar")
+	    );
+	    registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+	    	"JetRelCollectionName",
+		"Name of the Jet relation collection",
+		_colJetRelName,
+	    	std::string("FinalJets_rel")
+	    );
+	    _offsetCut = 0.05;
+	    registerProcessorParameter("offsetCut" , 
+	            "Offset cut for association"  ,
+        	    _offsetCut,
+           	 _offsetCut
+	    );
+	    _aParameter = 0.005;
+	    registerProcessorParameter("a" , 
+	            "a parameter of accuracy in mm"  ,
+        	    _aParameter,
+           	 _aParameter
+	    );
+	    _bParameter = 0.01;
+	    registerProcessorParameter("b" , 
+	            "b parameter of accuracy in mm"  ,
+        	    _bParameter,
+           	 _bParameter
+	    );
+	    
+		ip[0] = 0.0;
+		ip[1] = 0.0;
+		ip[2] = 0.0;
+	  
+	}
 
 
-VertexRestorer::VertexRestorer() : Processor("VertexRestorer") {
-  
-  // modify processor description
-  _description = "VertexRestorer";
-  
+	void VertexRestorer::init() 
+	{ 
+	   // usually a good idea to
+	   printParameters() ;
+	 
+	   _nRun = 0 ;
+	   _nEvt = 0 ;   
+	   
+	   hfile = new TFile( _hfilename.c_str(), "RECREATE", _hfilename.c_str() ) ;
+	 
+	   _Tree = new TTree( "Stats", "tree" );
+	   
+	   _Tree->Branch("detectedTotal", &_detectedTotal, "detectedTotal/I");
+	   _Tree->Branch("missedTotal", &_missedTotal, "missedTotal/I");
+	   _Tree->Branch("bstarTotal", &_bstarTotal, "bstarTotal/I");
+	   
+	   _Tree->Branch("missedDetected", &_missedDetected, "missedDetected/I");
+	   _Tree->Branch("missedMomentum", _missedMomentum, "missedMomentum[missedDetected]/F");
+	   _Tree->Branch("missedError", _missedError, "missedError[missedDetected]/F");
+	   _Tree->Branch("missedDeviation", _missedDeviation, "missedDeviation[missedDetected]/F");
+	   
+	   _Tree->Branch("bstarDetected", &_bstarDetected, "bstarDetected/I");
+	   _Tree->Branch("bstarMomentum", _bstarMomentum, "bstarMomentum[bstarDetected]/F");
+	   _Tree->Branch("bstarError", _bstarError, "bstarError[bstarDetected]/F");
+	   _Tree->Branch("bstarDeviation", _bstarDeviation, "bstarDeviation[bstarDetected]/F");
+	   
+	   _Tree->Branch("fakeDetected", &_fakeDetected, "fakeDetected/I");
+	   _Tree->Branch("fakeMomentum", _fakeMomentum, "fakeMomentum[fakeDetected]/F");
+	   _Tree->Branch("fakeError", _fakeError, "fakeError[fakeDetected]/F");
+	   _Tree->Branch("fakeDeviation", _fakeDeviation, "fakeDeviation[fakeDetected]/F");
 
-  // register steering parameters: name, description, class-variable, default value
-  
-  registerProcessorParameter( "ROOTFileName",
-                              "Output ROOT File Name",
-                              _hfilename,
-                              std::string("VertexRestorer.root") );
-  
-/*  registerInputCollection( LCIO::CALORIMETERHIT , 
-			   "CalorimeterHitCollection",
-			   "Name of the Calorimeter hit collection"  ,
-			   _colName ,
-			   std::string("ConvCalorimeterHits") ) ;
-*/
-  
-}
+	/*	std::cout << "|\tD0" bstar
+		       << "|\tZ0"  
+		       << "|\tPhi" 
+		       << "|\tOmega" 
+		       << "|\tTanl|" 
+		       << '\n';*/
+	}
 
-
-void VertexRestorer::init() 
-{ 
-   // usually a good idea to
-   printParameters() ;
- 
-   _nRun = 0 ;
-   _nEvt = 0 ;   
-   
-   hfile = new TFile( _hfilename.c_str(), "RECREATE", _hfilename.c_str() ) ;
- 
-   _Tree = new TTree( "tree", "tree" );
-   
-   _Tree->Branch("evtn", &_nEvt, "evtn/I");
-   _Tree->Branch("runN", &_nRun, "runN/I");
-
-}
-
-void VertexRestorer::processRunHeader( LCRunHeader* run) 
-{ 
-   _nRun++ ;
-} 
-
-void VertexRestorer::processEvent( LCEvent * evt ) 
-{ 
-	_nEvt ++ ;
-  
-	try 
+	void VertexRestorer::processRunHeader( LCRunHeader* run) 
+	{ 
+	   _nRun++ ;
+	} 
+	float VertexRestorer::GetDeviation(const EVENT::ReconstructedParticle * particle, double * pos)
 	{
+		float p = MathOperator::getModule(particle->getMomentum());
+		vector<float> direction = MathOperator::getDirection(particle->getMomentum());
+		vector<float> angles = MathOperator::getAngles(direction);
+		float accuracy = sqrt(_aParameter*_aParameter + _bParameter*_bParameter /( p * p * pow(sin(angles[1]), 4.0/3.0)) );
+		float offset = MathOperator::getDistanceTo(ip, direction, pos);
+
+		return offset / accuracy;
+	}
+	void  VertexRestorer::PrintTrack(Track * track)
+	{
+		if (!track) 
+		{
+			return;
+		}
+		std::cout << "|\t" << track->getD0() 
+		       << "|\t" << track->getZ0() 
+		       << "|\t" << track->getPhi() 
+		       << "|\t" << track->getOmega() 
+		       << "|\t" << track->getTanLambda()
+		       << '\n';
+	}
+
+		void VertexRestorer::processEvent( LCEvent * evt ) 
+		{ 
+			_nEvt ++ ;
+		  
+			try 
+			{
+				std::cout << "********VertexRecovery*"<<_nEvt << "**********\n";
+				//LCCollection* maincol = evt->getCollection( _colName );
+				LCCollection* seccol = evt->getCollection( _colSecName );
+				//vector < ReconstructedParticle * > * result = RestoreVerticesPFO(maincol, seccol);
+				LCCollection* pricol = evt->getCollection( _colPriName );
+				//LCCollection* jetcol = evt->getCollection( _colJetName );
+				//LCCollection* relcol = evt->getCollection( _colJetRelName );
+				Vertex * primary = dynamic_cast< Vertex * >(pricol->getElementAt(0));
+				vector < ReconstructedParticle * > * result = RestoreVertices(primary, seccol);
+				//vector < EVENT::Vertex * > * result = RestoreVertices(jetcol, relcol);
+				LCCollection* misscol = evt->getCollection( _colMissedName );
+				LCCollection* bstarcol = evt->getCollection( _colBStarName );
+				CompareCollections(result, misscol, bstarcol);
+				_Tree->Fill();
+			}
+			catch( DataNotAvailableException &e)
+			{
+			std::cout << "Whoops!....\n";
+			}
+		    
+		}
+
+	  	void VertexRestorer::CompareCollections(vector< ReconstructedParticle * > * detected, LCCollection * missed, LCCollection * bstar)
+		{
+			_missedTotal = missed->getNumberOfElements();
+			_bstarTotal = bstar->getNumberOfElements();
+			_detectedTotal = detected->size();
+
+			_missedDetected = 0;
+			_bstarDetected = 0;
+			_fakeDetected = 0;
+
+			for (int i = 0; i < _detectedTotal; i++) 
+			{
+				float taken = false;
+				ReconstructedParticle * detectedParticle = detected->at(i);
+				for (int j = 0; j < _missedTotal; j++) 
+				{
+					ReconstructedParticle * particle = dynamic_cast< ReconstructedParticle * > (missed->getElementAt(j));
+					if (CompareParticles(particle, detectedParticle)) 
+					{
+						taken = true;
+						//_missedDetected++;
+						_missedDeviation[_missedDetected] = GetDeviation(detectedParticle, MathOperator::toDoubleArray(detectedParticle->getStartVertex()->getPosition(), 3));
+						_missedError[_missedDetected] = GetError(detectedParticle);
+						_missedMomentum[_missedDetected++] = MathOperator::getModule(particle->getMomentum());
+						
+					}
+				}
+				for (int j = 0; j < _bstarTotal; j++) 
+				{
+					MCParticle * particle = dynamic_cast< MCParticle * > (bstar->getElementAt(j));
+					if (CompareParticles(particle, detectedParticle)) 
+					{
+						_bstarError[_bstarDetected] = GetError(detectedParticle);
+						//_bstarDetected++;
+						_bstarDeviation[_bstarDetected] = GetDeviation(detectedParticle, MathOperator::toDoubleArray(detectedParticle->getStartVertex()->getPosition(), 3));
+						taken = true;
+						_bstarMomentum[_bstarDetected++] = MathOperator::getModule(particle->getMomentum());
+					}
+				}
+				if (!taken) 
+				{
+					_fakeError[_fakeDetected] = GetError(detectedParticle);
+					_fakeDeviation[_fakeDetected] = GetDeviation(detectedParticle, MathOperator::toDoubleArray(detectedParticle->getStartVertex()->getPosition(), 3));
+					_fakeMomentum[_fakeDetected++] = MathOperator::getModule(detectedParticle->getMomentum());
+				}
+			}
+			//_fakeDetected = _detectedTotal - _missedDetected - _bstarDetected;
+			std::cout << "Total detected: " << _detectedTotal
+				<< "; missed total: " << _missedTotal
+				<< "; missed detected: " << _missedDetected
+				<< "; B* detected: " << _bstarDetected
+				<< '\n';
+
+		}
+
+	  	vector< ReconstructedParticle * > * VertexRestorer::RestoreVerticesPFO(LCCollection * main, EVENT::LCCollection * sec)
+		{
+			vector < ReconstructedParticle * > * result = new vector< ReconstructedParticle * >();
+			vector< ReconstructedParticle * > primaries;
+			int mnumber = main->getNumberOfElements();
+			for (int i = 0; i < mnumber; i++) 
+			{
+				primaries.push_back(dynamic_cast< ReconstructedParticle * > (main->getElementAt(i)));
+			}
+			int snumber = sec->getNumberOfElements();
+			vector< ReconstructedParticle * > * particles = new vector< ReconstructedParticle * >();
+			for (int i = 0; i < snumber; i++) 
+			{
+				Vertex * secondary = dynamic_cast< Vertex * >(sec->getElementAt(i));
+				vector< ReconstructedParticle * > secondaries = secondary->getAssociatedParticle()->getParticles();
+				particles->reserve(particles->size() + secondaries.size());
+				particles->insert(particles->end(),secondaries.begin(),secondaries.end());
+			}
+			for (int i = 0; i < snumber; i++) 
+			{
+				Vertex * secondary = dynamic_cast< Vertex * >(sec->getElementAt(i));
+				vector< ReconstructedParticle * > additional = GetAdditionalParticles(primaries, secondary, particles);
+				result->reserve(result->size() + additional.size());
+				result->insert(result->end(), additional.begin(),additional.end());
+			}
+			return result;
+
+		}
+
+		void VertexRestorer::PrintParticle(ReconstructedParticle * particle)
+		{
+			if (!particle) 
+			{
+				return;
+			}
+			std::cout << std::fixed << std::setw( 6 ) << std::setprecision( 3 ) << std::setfill( ' ' );
+			int id = 0;
+			if (particle->getParticleIDUsed()) 
+			{
+				std::cout << "Type " << particle->getParticleIDUsed()->getType() << '\n';
+				id = particle->getParticleIDs()[0]->getPDG(); 
+			}
+			std::cout<<"|"<< id <<"\t\t|"<<particle->getMass()<<"\t\t|"<<particle->getCharge()  <<"\t\t|"<<particle->getEnergy() <<"\t\t|\n";
+		}
+		bool VertexRestorer::CompareParticles(const ReconstructedParticle * particle1, const ReconstructedParticle * particle2)
+		{
+			if (particle1->getCharge() != particle2->getCharge()) 
+			{
+				return false;
+			}
+			float angle = MathOperator::getAngle(particle1->getMomentum(), particle2->getMomentum());
+			if (angle > 0.01) 
+			{
+				return false;
+			}
+			float recomodule = MathOperator::getModule(particle2->getMomentum());
+			float mcmodule = MathOperator::getModule(particle1->getMomentum());
+			float ratio = (1 - recomodule/mcmodule > 0.0) ? 1 - recomodule/mcmodule : recomodule/mcmodule - 1.0;
+			if (ratio > 0.02) 
+			{
+				return false;
+			}
+			return true;
+		}
+		bool VertexRestorer::CompareParticles(const MCParticle * particle1, const ReconstructedParticle * particle2)
+		{
+			if (particle1->getCharge() != particle2->getCharge()) 
+			{
+				return false;
+			}
+			float angle = MathOperator::getAngle(particle1->getMomentum(), particle2->getMomentum());
+			if (angle > 0.01) 
+			{
+				return false;
+			}
+			float recomodule = MathOperator::getModule(particle2->getMomentum());
+			float mcmodule = MathOperator::getModule(particle1->getMomentum());
+			float ratio = (1 - recomodule/mcmodule > 0.0) ? 1 - recomodule/mcmodule : recomodule/mcmodule - 1.0;
+			if (ratio > 0.02) 
+			{
+				return false;
+			}
+			return true;
+		}
+		vector < EVENT::ReconstructedParticle * > * VertexRestorer::RestoreVertices(Vertex * primary, LCCollection * sec)
+		{
+			//vector < EVENT::Vertex * > * result = new vector< EVENT::Vertex* >();
+			vector < ReconstructedParticle * > * result = new vector< ReconstructedParticle * >();
+			vector< ReconstructedParticle * > primaries = primary->getAssociatedParticle()->getParticles();
+			int number = sec->getNumberOfElements();
+
+			for (int i = 0; i < number; i++) 
+			{
+				Vertex * secondary = dynamic_cast< Vertex * >(sec->getElementAt(i));
+				vector< ReconstructedParticle * > additional = GetAdditionalParticles(primaries, secondary);
+				result->reserve(result->size() + additional.size());
+				result->insert(result->end(), additional.begin(),additional.end());
+			}
+			return result;
+		}
+		vector < EVENT::ReconstructedParticle * > * VertexRestorer::RestoreVertices(LCCollection * jetcol, LCCollection * rel)
+		{
+			vector < ReconstructedParticle * > * result = new vector< ReconstructedParticle * >();
+			int number = jetcol->getNumberOfElements();
+			LCRelationNavigator navigator(rel);
+			for (int i = 0; i < number; i++) 
+			{
+				ReconstructedParticle * jet = dynamic_cast<ReconstructedParticle *>(jetcol->getElementAt(i));
+				int nvtx = navigator.getRelatedToObjects(jet).size();
+				std::cout << "Jet energy: " << jet->getEnergy() 
+					 // << " b-tag: " << btag 
+					 // << " c-tag: " << ctag 
+					  << " # of vtx: " << nvtx 
+					  <<  '\n';
+				vector< LCObject * > objs = navigator.getRelatedToObjects(jet);
+				for (int j = 0; j < nvtx; j++) 
+				{
+					Vertex * vertex = dynamic_cast< Vertex * >(objs[j]);
+					vector< ReconstructedParticle * > additional = GetAdditionalParticles(jet->getParticles(), vertex);
+					result->reserve(result->size() + additional.size());
+					result->insert(result->end(), additional.begin(),additional.end());
+				}
+			}
+			return result;
+		}
+
+	  	vector< ReconstructedParticle * > VertexRestorer::GetAdditionalParticles(const vector< ReconstructedParticle * > & pri, Vertex * sec, const vector< ReconstructedParticle * > * toCompare)
+		{
+			vector< ReconstructedParticle * > result;
+			if (sec->getAssociatedParticle()->getParticles().size() < 2) 
+			{
+				return result;
+			}
+			const vector< ReconstructedParticle * > * particles = (toCompare)? toCompare : &(sec->getAssociatedParticle()->getParticles());
+			for (unsigned int i = 0; i < pri.size(); i++) 
+			{
+				ReconstructedParticle * primaryTrack = pri[i];
+				vector< float > direction = MathOperator::getDirection(primaryTrack->getMomentum());
+				double * pos = MathOperator::toDoubleArray(sec->getPosition(), 3);
+				float offset = MathOperator::getDistanceTo(ip, direction, pos);
+				float deviation = GetDeviation(primaryTrack, pos);
+				float angle = MathOperator::getAngle(primaryTrack->getMomentum(), pos);
+				if (abs(primaryTrack->getCharge()) > 0.0 
+				    && offset < _offsetCut  
+				    && angle < 0.05)
+				    //&& deviation < 10
+				    //&& !(MathOperator::getModule(primaryTrack->getMomentum()) > 5 && deviation > 6)) 
+				{
+					std::cout << "Found a track with offset " << offset 
+						<< ", deviation " << deviation 
+						<< ", angle " << angle
+						<< ", error*e-4 " << GetError(primaryTrack) * 10000
+						<<" :\n";
+					PrintParticle(primaryTrack);
+					bool found = false;
+					for (unsigned int j = 0; j < particles->size(); j++) 
+					{
+						ReconstructedParticle * secondary = particles->at(j);
+						if (CompareParticles(secondary, primaryTrack)) 
+						{
+							found = true;
+							break;
+						}
+					}
+					if (!found) 
+					{
+						std::cout << "The particle is unique!\n";
+						if (!IsDublicate(primaryTrack,result)) 
+						{
+							result.push_back(CopyParticle(primaryTrack,sec));
+						}
+					}
+				}
+			}
+			return result;
+		}
 		
-	}
-	catch( DataNotAvailableException &e)
-	{
-		std::cout << "Whoops!....\n";
-	}
-    
-}
-
+		ReconstructedParticle * VertexRestorer::CopyParticle(const ReconstructedParticle * particle, const Vertex * vertex)
+		{
+			ReconstructedParticleImpl * nouveau = new ReconstructedParticleImpl();
+			VertexImpl * vimp = new VertexImpl();
+			nouveau->setMomentum(particle->getMomentum());
+			nouveau->setMass(particle->getMass());
+			nouveau->setCharge(particle->getCharge());
+			nouveau->setEnergy(particle->getEnergy());
+			vimp->setPosition(vertex->getPosition());
+			nouveau->setStartVertex(vimp);
+			nouveau->addTrack(particle->getTracks()[0]); //CRUNCH
+			return nouveau;
+		}
+		float VertexRestorer::IsDublicate(const ReconstructedParticle * particle, vector< ReconstructedParticle * > & data)
+		{
+			bool dublicate = false;
+			for (unsigned int j = 0; j < data.size(); j++) 
+			{
+				if (CompareParticles(particle, data[j])) 
+				{
+					dublicate = true;
+					break;
+				}
+			}
+			return dublicate;
+		}
+		float VertexRestorer::GetError(const ReconstructedParticle * particle)
+		{
+			if (!particle || particle->getTracks().size() < 1) 
+			{
+				std::cout << "The particle is null or 0 tracks!\n";
+				return 0.0;
+			}
+			Track * track = particle->getTracks()[0];
+			return MathOperator::getTrace(track->getCovMatrix(), 5);
+		}
 
 void VertexRestorer::check( LCEvent * evt ) {}
 
@@ -74,7 +464,7 @@ void VertexRestorer::check( LCEvent * evt ) {}
 
 void VertexRestorer::end()
 {   
-  hfile->Write();
-  hfile->Close();
+hfile->Write();
+hfile->Close();
 }
-
+}
